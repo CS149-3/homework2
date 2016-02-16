2e68568e3aaf934b1332399c90458e37ed1ca2a8
Johnathon Ludeman <john@ludeman.com>
2016-02-16 15:19:02 -0800

    initial commit and framework

---
+++ homework2.c
@@ -0,0 +1,50 @@
+//
+//  homework2.c
+//  homework2
+//
+//  Created by Johnathon Ludeman on 2/16/16.
+//
+//
+
+#include <stdio.h>
+
+struct process {
+	float arrival;	// 0 - 99
+	float runtime;	// 0.1 - 10
+	int priority;	// 1, 2, 3, 4
+	char *name;	// A, B, C...
+};
+
+struct output {
+	char *timechart;
+	float avgturnaround;
+	float avgwaiting;
+	float avgresponse;
+};
+
+struct process processes[100];
+
+int main() {
+	// generate processes (processes should only be generated once!)
+	
+	
+	// 1. run First-come first served
+	
+	
+	// 2. run Shortest job first
+	
+	
+	// 3. run Shortest remaining time
+	
+	
+	// 4. run Round robin
+	
+	
+	// 5. run Highest priority first (preemetive)
+	
+	
+	// 6. run Highest priority first (non-preemptive)
+	
+	
+	return 0;
+}
