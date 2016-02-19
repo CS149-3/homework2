280828a9c49516f5e5b8024aec0d61751d456388
Johnathon Ludeman <john@ludeman.com>
2016-02-18 17:59:46 -0800

    converted to java framework, multiple classes to avoid git mistakes

---
+++ ShortestRemainingTime.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+
+public class ShortestRemainingTime implements Executable {
+
+	@Override
+	public Result execute(ArrayList<Process> processes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Simulation.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Simulation {
+	
+	public static ArrayList<Process> processes = new ArrayList<Process>();
+	
+	public static void main(String[] args) {
+		// generate processes (processes should only be generated once!)
+		generateProcesses(20);
+		
+		// 1. run First-come first served
+		Result fcfs = new FirstComeFirstServed().execute(processes);
+		
+		resetProcesses();
+		
+		// 2. run Shortest job first
+		Result sjf = new ShortestJobFirst().execute(processes);
+		
+		// 3. run Shortest remaining time
+		Result smt = new ShortestRemainingTime().execute(processes);
+		
+		// 4. run Round robin
+		Result rr = new RoundRobin().execute(processes);
+		
+		// 5. run Highest priority first (preemptive)
+		Result hpfp = new HighestPriorityFirstPreemptive().execute(processes);
+		
+		// 6. run Highest priority first (non-preemptive)
+		Result hpfnp = new HighestPriorityFirstNonPreemptive().execute(processes);
+		
+		//print results (if we don't just change the above to execute in println's)
+		
+	}
+
+	private static void generateProcesses(int numProcesses) {
+		if (numProcesses < 1 || numProcesses > 26) throw new RuntimeException("Process count must be between 1 and 26");
+		
+		Random random = new Random();
+		
+		for (int i = 0; i < numProcesses; i++) {
+			processes.add(new Process(Character.toString((char) ('A' + i)), random.nextInt(4) + 1, random.nextFloat() * 100, random.nextFloat() * 10 + 0.1f));
+		}
+	}
+	
+	private static void resetProcesses() {
+		for (Process process : processes) process.reset();
+	}
+}
