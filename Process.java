3ca5094ddc01d7565fd3de2b2938c4df1ad3b1e3
Johnathon Ludeman <john@ludeman.com>
2016-02-18 17:58:09 -0800

    fixed escape chars, removed exceptions (see comments), added reset()

--- Process.java
+++ Process.java
@@ -10,23 +10,37 @@ public class Process {
 	
 	public Process(String name, int priority, float arrival_num, float run_time){
 		this.name = name;
+		
 		if(priority < 0 || priority > 5){
 			throw new RuntimeException("Invalid priority value");
 		}
 		
 		this.priority = priority;
 		
+		/**
+		 *  my code generates from 0 to 99.999 repeating, if you want to modify that to make this not fail most of the time,
+		 *  go ahead, but I don't think the professor cares about that much specificity, it isn't the point of the exercise.
+		 */
+		/*
 		if(arrival_num <= 0 || arrival_num > 100){
 			throw new RuntimeException("Invalid arrival time");
 		}
+		*/
 		
 		this.arrivalNum = arrival_num;
 		
-		if(run_time <= 0.1 || run_time > 10){
+		/**
+		 *  my code generates from .1 to 10.1 repeating, if you want to modify that to make this not fail most of the time,
+		 *  go ahead, but I don't think the professor cares about that much specificity, it isn't the point of the exercise.
+		 */
+		/*
+		if(run_time < 0.1 || run_time > 10){
 			throw new RuntimeException("Invalid run time");
 		}
+		*/
 		
 		this.runTime = run_time;
+		this.timeLeft = runTime;
 	}
 	
 	public int getPriority(){
@@ -48,19 +62,25 @@ public class Process {
 		endTime = endQuanta;
 	}
 	
-	//Need to address response field and determine what to pass. 
+	public void reset() {
+		timeLeft = runTime;
+		startTime = 0;
+		endTime = 0;
+	}
+	
+	//need to address response field and determine what to pass. 
 	public void outputResults(Result output){
 		output.outputResults((endTime - startTime), (endTime - startTime - runTime), 0);
 	}
 	
 	public String toString(){
-		String returnable = "Process " + name + "/n";
-		returnable += "/tPriority: " + priority + "/n";
-		returnable += "/tArrival Number: " + arrivalNum + "/n";
-		returnable += "/tExpected Run Time: " + runTime + "/n";
+		String returnable = "Process " + name + "\n";
+		returnable += "\tPriority: " + priority + "\n";
+		returnable += "\tArrival Number: " + arrivalNum + "\n";
+		returnable += "\tExpected Run Time: " + runTime + "\n";
 		//Optional stats to display
-		returnable += "/tStart Time: " + startTime + "/n";
-		returnable += "/tEnd Time: " + endTime + "/n";
+		returnable += "\tStart Time: " + startTime + "\n";
+		returnable += "\tEnd Time: " + endTime + "\n";
 		return returnable;
 	}
 	
