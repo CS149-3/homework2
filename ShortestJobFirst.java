7dc0831ba6c4d747e85dfc14d48f702e69f35c74
Frank Mock <FMock@users.noreply.github.com>
2016-02-19 17:30:34 -0800

    Updated ShortestJobFirst.java
    Added sorting of the processes by the estimated run time which is required by the algorithm

--- ShortestJobFirst.java
+++ ShortestJobFirst.java
@@ -1,14 +1,23 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 /*
-* Non-Preemptive batch algoritm that assumes the run times are known
+* Non-Preemptive batch algorithm that assumes the run times are known
 * in advance.
 */
-public class ShortestJobFirst implements Executable {
+public class ShortestJobFirst implements Executable
+{
 
 	@Override
-	public Result execute(ArrayList<Process> processes) {
-		// TODO Auto-generated method stub
+	public Result execute(ArrayList<Process> processes) 
+	{
+		//Sort the list of Processes by estimated run time
+		Collections.sort(processes, new Process());
+		
+		for(Process s : processes)
+		{
+			System.out.println(s.toString());
+		}
 		return null;
 	}
 
