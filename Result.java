b3c761fc4b257b7c67175f280ce9c2b82b7d2bfc
isabelchan <chanisabelbel@yahoo.com>
2016-02-19 22:10:24 -0800

    Update Result.java
    result is similar to update-result branch, but made processList output the arraylist of processes

--- Result.java
+++ Result.java
@@ -3,44 +3,45 @@ import java.util.ArrayList;
 public class Result {
 	
 	private ArrayList<String> timeChart;
-	private ArrayList<String> processList;
-	private float turnaroundTotal;
-	private float waitingTotal;
-	private float responseTotal;
+	private ArrayList<Process> processList;
+	private float averageTurnaround;
+	private float averageWaiting;
+	private float averageResponseTime;
 	
 	
 	public Result(){
 		timeChart = new ArrayList<String>();
-		processList = new ArrayList<String>();
-		turnaroundTotal = 0;
-		waitingTotal = 0;
-		responseTotal = 0;
+		processList = new ArrayList<Process>();
+		averageTurnaround = 0;
+		averageWaiting = 0;
+		averageResponseTime = 0;
 	}
 	
 	public void addChart(String processes){
 		timeChart.add(processes);
 	}
 	
-	public void addProcess(String process){
+	public void addProcess(Process process){
 		processList.add(process);
 	}
 	
-	public void outputResults(float turnaround, float waiting, float response){
-		turnaroundTotal += turnaround;
-		waitingTotal += waiting;
-		responseTotal += response;
+	public void runResults(float turnaround, float waiting, float response){
+		averageTurnaround = turnaround;
+		averageWaiting = waiting;
+		averageResponseTime = response;
 	}
 	
 	public String toString(){
-		String returnable = "Process List: /n";
+		String returnable = "Process List: ";
 		
 		for(int i = 0; i < processList.size(); i++){
-			returnable += processList.get(i) + "/n";
+			returnable += processList.get(i);
 		}
+		returnable += "\nTime Chart:\n" + timeChart.toString() + "\n";
+		returnable += "Turnaround Avg: " + averageTurnaround +"\n";
+		returnable += "Waiting Avg: " + averageWaiting + "\n";
+		returnable += "Response Avg:" + averageResponseTime + "\n";
 		
-		returnable += "Turnaround Avg: " + (turnaroundTotal / processList.size()) +"\n";
-		returnable += "Waiting Avg: " + (waitingTotal / processList.size()) + "\n";
-		returnable += "response Avg:" + (responseTotal / processList.size()) + "\n";
 		return returnable; 
 	}
 }
