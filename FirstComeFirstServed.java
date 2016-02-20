c833f9c6c5241d2622eb08946729ad32bfc14ad9
isabelchan <chanisabelbel@yahoo.com>
2016-02-19 18:31:50 -0800

    Update FirstComeFirstServed.java

--- FirstComeFirstServed.java
+++ FirstComeFirstServed.java
@@ -1,22 +1,10 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.*;
-
 
 public class FirstComeFirstServed implements Executable{
 	
-	@Override
 	public Result execute(ArrayList<Process> processes) {
+		processes.sort(Process.compareByArrival());
+		System.out.println(processes);
 		return null;
 	}
-	
-	public void print(ArrayList<Process> processes){
-		System.out.println("size: "+ processes.size());
-		Collections.sort(processes);
-		//sort(processes);
-		for(int i=0;i<processes.size();i++){
-			System.out.println(processes.get(i));
-		}
-	}	
 }
