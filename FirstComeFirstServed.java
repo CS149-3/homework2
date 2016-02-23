d5d072ca2caf2b3c3b307d3e276333f3a19514b0
isabelchan <chanisabelbel@yahoo.com>
2016-02-22 16:34:12 -0800

    Update FirstComeFirstServed.java
    took out result.addProcess

--- FirstComeFirstServed.java
+++ FirstComeFirstServed.java
@@ -15,7 +15,7 @@ public class FirstComeFirstServed implements Executable{
 		processes.sort(Process.compareByArrival());
 		ArrayList<String> quanta = quantaForTimeChart(processes);
 		for(int i=0;i<processes.size();i++){
-			result.addProcess(processes.get(i));
+			//result.addProcess(processes.get(i));
 			//result.addChart(processes.get(i).getName());
 			turnAroundTotal=turnAroundTotal+turnAroundTotal+processes.get(i).getRunTime();
 			averageTurnAround=turnAroundTotal/(i+1);
